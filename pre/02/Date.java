1a9ee799378ea8ce91a3654cb2646bc971fd6414
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-09-27 18:40:31 -0400

    Add files via upload

---
+++ pre/02/Date.java
@@ -0,0 +1,16 @@
+// Exercise 3 of Chapter 2
+
+public class Date{
+
+      public static void main(String[] args){
+          String day = "Thursday";
+          int date = 18;
+          String month = "July";
+          int year = 2015;
+
+          System.out.print("American format:" + "\n");
+          System.out.print(day + ", " + month + " " + date + ", " + year + "\n");
+          System.out.print("European format:" + "\n");
+          System.out.println(day + " " + date + " " + month + " " + year + "\n");
+      }
+}
---
+++ pre/02/TimeConversion.java
@@ -0,0 +1,14 @@
+// Exercise 3 of Chapter 3
+
+public class TimeConversion{
+
+      public static void main(String[] args){
+          int TotalNumberOfSeconds = 9065;
+          int seconds = 9065 % 60;
+          int minutes = 9065 / 60 % 60;
+          int hours = 9065 / 60 / 60 % 24;
+
+          System.out.print(TotalNumberOfSeconds + "seconds = ");
+          System.out.print(hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
+      }
+}
