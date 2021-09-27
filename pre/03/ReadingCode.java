5daf0318a2d7fda299552b05ffd2a15d73c70867
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-09-27 18:41:03 -0400

    Add files via upload

---
+++ pre/03/ReadingCode.java
@@ -0,0 +1,10 @@
+// Exercise 1 from Chapter 4
+
+/**
+1. baffle prints "wug" and invokes ping. Ping prints "."
+No, I wug.You wugga wug.I wug.
+2. main() -> "No, I"
+  zoop() 
+  baffle() -> "wug"
+  ping()
+3. If you invoke baffle() at the end of ping(), there will be an infinite loop.
