b053f3a34c0d69dfe90f9143485b408906973b27
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-09-27 18:52:51 -0400

    Add files via upload

---
+++ pre/07/Powers.java
@@ -0,0 +1,44 @@
+// Exercises 1, 4, 5 from Chapter 8
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Powers{
+
+  public static void main(String[] args){
+    double[] values = new double[] {3, 4, 5, 6};
+    double[] newArray = Arrays.copyOf(values, 4);
+    System.out.println(powArray(newArray, 2));
+    System.out.println(histogram(100));
+  }
+
+// Exercise 1
+  public static String powArray(double[] a, int power){
+    for (int i = 0; i < a.length; i++){
+      a[i] = Math.pow(a[i], power);
+    }
+    return Arrays.toString(a);
+  }
+
+  public static int inRange(int[] masterlist, int low, int high){
+    int count = 0;
+    for (int i = 0; i < masterlist.length; i++){
+      if (masterlist[i] > low && masterlist[i] < high) {
+        count ++;
+      }
+    }
+  }
+
+  public static int[] histogram(int counter){
+    Random random = new Random();
+    int[] masterlist = new int[counter];
+    for (int i = 0; i < masterlist.length; i++){
+      masterlist[i] = random.nextInt(100);
+    }
+    for (int i = 0; i < masterlist.length; i++){
+      masterlist[i] = inRange(, i, i + 1);
+    }
+
+  }
+
+}
