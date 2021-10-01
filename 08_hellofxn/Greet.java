c57fc7d3af3f781dd45efd88a8af5aa80bc9ce0d
JeffordShau <jshau30@stuy.edu>
2021-09-30 21:05:00 -0400

    final version

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,22 @@
+/*
+Jefford Shau
+APCS
+HW08 -- Helper Funtions
+2021-09-30
+*/
+
+/*
+Discoveries: The invocation of the functionhas the same syntax as Python..
+Unresolved Questions: null
+*/
+
+public  class Greet {
+	public static void main (String[] args){
+		greet("John");
+		greet("James");
+		greet("Amy"); 
+	}
+	public static void greet(String name){
+		System.out.println("Hello there, " + name + "! Is all well?");
+	}
+}
