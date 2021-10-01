e2a101827ca6bb344fa4bfab1bed539e8b0033bb
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-01 14:01:38 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,1 +1,17 @@
+/*
+Jefford Shau
+APCS
+HW 09 -- Greet v2
+2021-10-01
+*/
 
+/*
+Discoveries: 
+Unresolved Questions: 
+*/
+
+public class BigSib {
+	 public static void greet(String name){
+		System.out.println("Hello there, " + name + "! Is all well?");
+	}
+}
