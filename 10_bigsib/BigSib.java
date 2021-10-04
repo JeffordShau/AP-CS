bba9c9cdabad8337c2af7809101ee3778ace6caa
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-04 18:13:45 -0400

    Create BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,17 @@
+/*
+BeepBoop: Jefford & Dylan, Anthony & Corn, Jeffery & Matthias
+APCS
+HW10 -- Return BigSib to SOP Greet
+2021-10-04
+*/
+
+/*
+Discoveries: 
+Unresolved Questions: 
+*/
+
+public class BigSib {
+	 public static void greet(String name){
+		return("Hello there, " + name + "! Is all well?");
+	}
+}
