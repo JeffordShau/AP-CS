0da45ae8d5d23f632d5bf6fd6565237f7f157550
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-04 18:12:39 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,5 +1,19 @@
 /*
 BeepBoop: Jefford & Dylan, Anthony & Corn, Jeffery & Matthias
 APCS
-HW10 -- 
-<yyyy>-<mm>-<dd>
+HW10 -- Return BigSib to SOP Greet
+2021-10-04
+*/
+
+/*
+Discoveries: 
+Unresolved Questions: 
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
