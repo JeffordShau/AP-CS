1f915ed02a92f43966d879cb6cf0d4bc715c3244
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-05 19:46:18 -0400

    Create Greet.java

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,24 @@
+/*
+Jefford Shau
+APCS
+HW12 -  Greet v3
+2021-10-05
+*/
+
+/*
+DISCO: 
+QCC: 
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
