edecd937f9755b388234f1a105767dea23aae432
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-05 20:13:08 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,28 @@
+/*
+Jefford Shau
+APCS
+HW12 -- Instance Variable
+2021-10-05
+*/
+
+/*
+DISCO: 
+QCC: 
+*/
+
+public class BigSib {
+	 public static String greet(String name){
+		return setHelloMsg(name);
+	}
+}
+  
+  public static String setHelloMsg(String name){
+    String helloMsg; 
+    helloMsg += name
+ /*
+$ javac Greet.java
+Word up freshman
+Salutations Dr. Spaceman
+Hey ya Kong Fooey
+Sup mom
+*/
