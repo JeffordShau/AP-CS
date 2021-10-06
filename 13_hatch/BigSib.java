ab34198bdad47c797f464167b2ad827d36391017
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-06 15:35:19 -0400

    Create BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,23 @@
+/*
+Triple J: Jeffery Tang & Mathias, Jefford Shau & Dylan, Jun Hong Wang & Bob
+APCS
+HW12 -- Instance Variables
+2021-10-05
+*/
+
+/*
+DISCO: The helloMsg instance variable must be defined outside of both methods and inside of the class BigSib to be used by the greet method and the setHelloMsg method. 
+Defining the "richard" variable as the BigSib function worked smoothly. 
+QCC: Why use "richard" variable instead of BigSib. ?
+*/
+
+public class BigSib {
+	String helloMsg; 
+	public String greet(String name){
+		return helloMsg + " " + name;
+	}
+  
+  public void setHelloMsg(String MsgPhrase){
+    helloMsg = MsgPhrase;
+	}
+}
