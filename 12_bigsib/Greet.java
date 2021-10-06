bf2e58ef85b4723089dda44b5183dc419da318ee
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-05 20:13:11 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -11,14 +11,28 @@ QCC:
 */
 
 public class Greet {
-  public static void main( String[] args ) {
-    String greeting;
+    public static void main( String[] args ){
+        String greeting;
+        BigSib richard = new BigSib();
 
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+        richard.setHelloMsg("Word up");
+        greeting = richard.greet("freshman");
+        System.out.println(greeting);
 
-    greeting = richard.greet("freshman");
-    System.out.println(greeting);
+        richard.setHelloMsg("Salutations");
+        greeting = richard.greet("Dr. Spaceman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Hey ya");
+        greeting = richard.greet("Kong Fooey");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Sup");
+        greeting = richard.greet("mom");
+        System.out.println(greeting);
 
   } //end main()
 } //end Greet
+
+
+Revise class BigSib to work with class Greet by adding instance variable helloMsg and a method to set its value. Output should look like this:
