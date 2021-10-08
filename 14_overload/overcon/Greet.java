a4df9aeca54a0bcf8e84f769f2beb8e91802da9d
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-07 20:02:22 -0400

    Update Greet.java

--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -9,3 +9,22 @@ HW14 -  Default Constructor
 DISCO: 
 QCC: 
 */
+
+public class Greet {
+public static void main( String[] args) {
+  String greeting;
+  BigSib richard = new BigSib("Word up");
+  BigSib grizz = new BigSib("Hey ya");
+  BigSib dotCom = new BigSib("Sup");
+  BigSib tracy = new BigSib("Saluations");
+  greeting = richard.greet("freshman");
+  System.out.println(greeting);
+  greeting = tracy.greet("Dr. Spaceman");
+  System.out.println(greeting);
+  greeting = grizz.greet("Kong Fooey");
+  System.out.println(greeting);
+  greeting = dotCom.greet("mom");
+  System.out.println(greeting);
+}
+}
+  
