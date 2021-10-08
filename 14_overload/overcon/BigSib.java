8c1e4226f636aac2203de76ccbe4e4debdf68ca5
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-07 21:06:18 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -6,6 +6,19 @@ HW14 -  Default Constructor
 */
 
 /*
-DISCO: 
-QCC: 
+DISCO: got an error: error: cannot find symbol. this made me notice that the variable had to be defined before the default constructor and not inside. 
+QCC: why can you not define variables inside the default constructor if the default constructor is accessible by the other methods anyway? 
 */
+
+public class BigSib {
+	String helloMsg; 
+ 	public BigSib() {
+		helloMsg = "Nice to meet you, ";
+	}
+	public BigSib(String msg){
+		helloMsg = msg; 
+	}
+	public String greet(String name){
+		return helloMsg + " " + name;
+	}
+}
