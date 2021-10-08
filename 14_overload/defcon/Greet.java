c7d21a37ec277fd93c34f38bf6188d56bee8a204
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-07 20:00:28 -0400

    Update and rename 10-07CW/Greet.java to 14_overload/defcon/Greet.java

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -1,3 +1,15 @@
+/*
+Triple J: Jeffery Tang & Mathias, Jefford Shau & Dylan, Jun Hong Wang & Bob
+APCS
+HW14 -  Default Constructor
+2021-10-07
+*/
+
+/*
+DISCO: 
+QCC: 
+*/
+
 public class Greet {
 public static void main( String[] args) {
   String greeting;
