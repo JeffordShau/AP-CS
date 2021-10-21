22adee14266439cb59426c52ac7301fa526b0458
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-21 00:44:46 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -11,20 +11,9 @@ UNRESOLVED QUESTIONS
     1. How can we convert a double to an int without type casting?
 */
 
-/**
-Skeleton from: 
-    Clyde "Thluffy" Sinclair
-    APCS
-    HW20 --
-    2021-10-21
-    xtra: harmonic mean is the reciprocal of the
-    arithmetic mean of the reciprocals
-**/
-
-import java.util.Math;
+import java.lang.Math;
 
 public class Stats {
-
     public static int mean(int a, int b) {
         return (a + b) / 2;
     }
@@ -50,11 +39,21 @@ public class Stats {
     }
 
     public static int geoMean(int a, int b) {
-        return Math.sqrt(a * b);
+        if (a >= 0 && b >= 0) {
+           return Math.pow(a * b, 1/2);
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1;
+        }
     }
 
     public static double geoMean(double a, double b) {
-        return Math.sqrt(a * b);
+        if (a >= 0 && b >= 0) {
+            return Math.pow(a * b, 1/2);
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1.0;
+        }
     }
 
     public static int max(int a, int b, int c) {
@@ -78,53 +77,23 @@ public class Stats {
     }
 
     public static int geoMean(int a, int b, int c) {
-
+        if (a >= 0 && b >= 0) {
+            return Math.pow(a * b * c, 1/3);
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1;
+        }
     }
 
     public static double geoMean(double a, double b, double c) {
-
+        if (a >= 0 && b >= 0) {
+            return Math.pow(a * b * c, 1/3);
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1;
+        }
     }
 
-
-    //main method for testing functionality
-    public static void main( String[] args ) {
-        Stats s = new Stats();
-        // int mean()
-        System.out.println(s.mean(2, 2)); // 2
-        System.out.println(s.mean(2, 4)); // 3
-        System.out.println(s.mean(0, 0)); // 0
-        System.out.println(s.mean(0, -2)); // -1
-        System.out.println(s.mean(-4, -2)); // -3
-        // double mean()
-        System.out.println(s.mean(2.0, 2.0)); // 2.0
-        System.out.println(s.mean(2.0, 4.0)); // 3.0
-        System.out.println(s.mean(0.0, 0.0)); // 0.0
-        System.out.println(s.mean(0.0, -2.0)); // -1.0
-        System.out.println(s.mean(-4.0, -2.0)); // -3.0
-        // int max()
-        System.out.println(s.max(2, 2)); // 2
-        System.out.println(s.max(2, 4)); // 4
-        System.out.println(s.max(0, 0)); // 0
-        System.out.println(s.max(0, -2)); // 0
-        System.out.println(s.max(-4, -2)); // -2
-        // double max()
-        System.out.println(s.max(2.0, 2.0)); // 2.0
-        System.out.println(s.max(2.0, 4.0)); // 4.0
-        System.out.println(s.max(0.0, 0.0)); // 0.0
-        System.out.println(s.max(0.0, -2.0)); // 0.0
-        System.out.println(s.max(-4.0, -2.0)); // -2.0
-        // int geoMean()
-        System.out.println(s.geoMean(18, 18)); // 18
-        System.out.println(s.geoMean(2, 18)); // 6
-        System.out.println(s.geoMean(0, 0)); // 0
-        System.out.println(s.geoMean(-2, 18)); // error: cannot handle imaginary numbers
-        System.out.println(s.geoMean(-2, -18)); // 6
-        // double geoMean()
-        System.out.println(s.geoMean(18.0, 18.0)); // 18.0
-        System.out.println(s.geoMean(2.0, 18.0)); // 6.0
-        System.out.println(s.geoMean(0.0, 0.0)); // 0.0
-        System.out.println(s.geoMean(-2.0, 18.0)); // error: cannot handle imaginary numbers
-        System.out.println(s.geoMean(-2.0, -18.0)); // 6.0
     }
 
 }//end class
