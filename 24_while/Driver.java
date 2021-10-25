a14d02512087b342f67c29137941d8802b13c6dc
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-25 16:46:47 -0400

    Create Driver.java

---
+++ 24_while/Driver.java
@@ -0,0 +1,96 @@
+/*
+JJ: (Jefford Shau and Jomin Zhang )
+APCS
+HW23 -- Coins
+2020-10-22
+time spent: 1.1 hours
+ */
+
+/*
+DISCO:
+  0. Implemented the Math.random into my code to randomize the coin flips.
+  1. "this" keyword refers to the current object in the invocation of the Driver line.
+  2. Found that using Math.random was easier to use than importing random class.
+QCC:
+  0. How do you check if an object exists without producing an error?
+  1. When is it better to import the random class than use Math.random?
+*/
+
+/***
+    driver for class Coin
+    ~~~ SUGGESTED WORKFLOW: ~~~
+    1. Compile this file and run. Note anything notable.
+    2. Move the "TOP" line down, so that it is below the first statement.
+    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
+    (your editor: ???)
+    3. Compile and run again.
+    4. Resolve errors one at a time until it works.
+    5. Repeat 2-4 until TOP meets BOTTOM.
+***/
+
+public class Driver {
+
+  public static void main( String[] args ) {
+
+    //build Objects from blueprint specified by class Coin
+
+
+    //test default constructor
+      Coin mine = new Coin();
+      //test 1st overloaded constructor
+      Coin yours = new Coin( "quarter" );
+      //test 2nd overloaded constructor
+      Coin wayne = new Coin( "dollar", "heads" );
+      //test toString() methods of each Coin
+      System.out.println("mine: " + mine);
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test flip() method
+      System.out.println("\nAfter flipping...");
+      yours.flip();
+      wayne.flip();
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+
+      //test equals() method
+      if ( yours.equals(wayne) ) {
+        System.out.println( "Matchee matchee!" );
+      }
+      else {
+        System.out.println( "No match. Firestarter you can not be." );
+      }
+
+      // Flip until x amount of heads appears
+      int counter = 0;
+      while (counter < 10) {
+        if (yours.flip() == "heads"){
+          counter += 1;
+          System.out.println("Heads: "+ yours.getHeadsCtr());
+        }
+      }
+      //Flip until y amount of matches
+      counter = 0;
+      while (counter < 10){
+        yours.flip();
+        wayne.flip();
+        if ( yours.equals(wayne) ) {
+          counter +=1;
+          System.out.println("Matchee matchee!");
+        }
+      }
+      //Flip until 65536 matches have occured and hte number of matches is divisible by the year of your birth
+      counter = 0;
+      while ((counter < 65536) && (counter % 2004 == 0)){
+        yours.flip();
+        wayne.flip();
+        if ( yours.equals(wayne) ) {
+          counter +=1;
+        }
+      }
+      System.out.println(counter);  
+
+
+
+  }//end main()
+
+}//end class
