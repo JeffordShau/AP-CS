f7ff5b05c85cdd7a2e7e3a4bbcad2d014cdbc1e8
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-27 20:53:05 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -1,78 +1,64 @@
-/*
-AJR (Jefford Shau, Ameer Alnasser, Ryan Lau)
-APCS
-HW26 -- GCD
-2020-10-27
-time spent: 
-*/
+// TNPG: AJR (Ameer Alnasser, Jefford Shau, Ryan Lau)
+// APCS
+// HW26 -- GCD Three Ways
+// 2021-10-27
+// time spent: 0.5 hrs
 
 /*
-DISCO: 
-
-QCC: 
-
-ALGO: 
-
+DISCOVERIES
+UNRESOLVED QUESTIONS
+ALGO
 */
 
-/*
-precond: a and b are positive integers
-*/
-public class Stats{
+import java.lang.Math;
 
-  public static int gcd(int a, int b){
-	  int counter = 0;
-		int GCD;
-	  if (a > b) {
-		  int smaller = a; 
-	  } else {
-		  int smaller = b;
-	  }
-	  while (counter < smaller) {
-		counter += 1;
-		if (a % counter == 0 && b % counter == 0) {
-			GCD = counter;
-		}
-	}
-	return GCD;
-  }
-  
-	 public static int gcdER(int a, int b) {
-		if (a == b) {
-			return a;
-		} else {
-		if (a < b) {
-		  gcd(a , b - a);
-		} else {
-		  gcd(a - b, b);
+public class Stats {
+    public static int gcd (int a, int b){
+        int counter = 0;
+        int gcd = 0;
+        while (counter < (a + b)) {
+            counter++;
+            if (a % counter == 0 && b % counter == 0) {
+                gcd = counter;
+            }
+        }
+        return gcd;
     }
+
+    public static int gcdER(int a, int b) {
+        if (a == b) {
+            return a;
+        }
+        if (a < b) {
+            return gcd(a, b - a);
+        } else {
+            return gcd(a - b, b);
+        }
     }
-   }
-  
-	public static int gcdEW(int a, int b) {
-		while (a != b) {
-			if (a > b) {
-				a = a - b;
-			} else {
-				b = b - a;
-			}
-		}
-		return a;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(gcd(15, 38));
-		System.out.println(gcd(9, 9));
-		System.out.println(gcd(1, 2));
-		System.out.println(gcd(1, 1));
-		System.out.println(gcdER(15, 38));
-		System.out.println(gcdER(9, 9));
-		System.out.println(gcdER(1, 2));
-		System.out.println(gcdER(1, 1));
-		System.out.println(gcdEW(15, 38));
-		System.out.println(gcdEW(9, 9));
-		System.out.println(gcdEW(1, 2));
-		System.out.println(gcdEW(1, 1));
-	
-	}
-}
+
+    public static int gcdEW(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("testing brute force algo:");
+        System.out.println("the gcd of 15 and 38 is: " + gcd(15, 38));
+        System.out.println("the gcd of 15 and 30 is: " + gcd(15, 30));
+        System.out.println("the gcd of 15 and 15 is: " + gcd(15, 15));
+
+        System.out.println("\ntesting recursive algo:");
+        System.out.println("the gcd of 15 and 38 is: " + gcdER(15, 38));
+        System.out.println("the gcd of 15 and 30 is: " + gcdER(15, 30));
+        System.out.println("the gcd of 15 and 15 is: " + gcdER(15, 15));
+
+        System.out.println("\ntesting while algo:");
+        System.out.println("the gcd of 15 and 38 is: " + gcdEW(15, 38));
+        System.out.println("the gcd of 15 and 30 is: " + gcdEW(15, 30));
+        System.out.println("the gcd of 15 and 15 is: " + gcdEW(15, 15));
