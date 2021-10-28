d449c018f87a03d363e6a9a056d5c71aa373de33
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-27 21:50:11 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -6,24 +6,20 @@
 
 /*
 DISCOVERIES
-    0. The brute-force method required more work and code than the recursive and the while loop methods. 
-    1. This method of GCD does not work with zero or negative numbers, so we set the pre-condition of a and b to accept positive integers only. 
+    0. The brute-force method required more work and code than the recursive and the while loop methods.
+    1. This method of GCD does not work with zero or negative numbers, so we set the pre-condition of a and b to accept positive integers only.
 UNRESOLVED QUESTIONS
     0. Is there a more efficient method in finding gcd?
 ALGO
-    0. Our gcdER(int, int) method works as follows:
-
-The base case for the algorithm is if the two inputs are equal to each other due to the fact that we can't subtract a lesser number from a greater number if they are the same. In this scenario, we just return one of the inputs as the GCD.
-
-Based on the fact that the GCD of two numbers is the same as the GCD of the smaller number subtracted from the greater number and the smaller number itself, we will check if the first input is greater than the second. If it is, we will return a GCD method call where the inputs are the second input and the second input subtracted from the first. If the second input is greater, we will return a GCD method call where the inputs are the first input and the first input subtracted from the second.
-
-From here, the GCD method will be recursively run, first checking if both inputs are the same and so on.
+    Our gcdER(int, int) method works as follows:
+    The base case for the algorithm is if the two inputs are equal to each other due to the fact that we can't subtract a lesser number from a greater number if they are the same. In this scenario, we just return one of the inputs as the GCD.
+    Based on the fact that the GCD of two numbers is the same as the GCD of the smaller number subtracted from the greater number and the smaller number itself, we will check if the first input is greater than the second. If it is, we will return a GCD method call where the inputs are the second input and the second input subtracted from the first. If the second input is greater, we will return a GCD method call where the inputs are the first input and the first input subtracted from the second.
+    From here, the GCD method will be recursively run, first checking if both inputs are the same and so on.
 */
 
 import java.lang.Math;
 
 public class Stats {
-    
     public static int mean(int a, int b) {
         return (a + b) / 2;
     }
@@ -109,7 +105,7 @@ public class Stats {
             return -1;
         }
     }
-    
+
     public static int gcd (int a, int b){
         int counter = 0;
         int gcd = 0;
@@ -133,9 +129,9 @@ public class Stats {
             return a;
         }
         if (a < b) {
-            return gcd(a, b - a);
+            return gcdER(a, b - a);
         } else {
-            return gcd(a - b, b);
+            return gcdER(a - b, b);
         }
     }
 
@@ -166,4 +162,4 @@ public class Stats {
         System.out.println("the gcd of 15 and 30 is: " + gcdEW(15, 30));
         System.out.println("the gcd of 15 and 15 is: " + gcdEW(15, 15));
     }
-}
+}//end class
