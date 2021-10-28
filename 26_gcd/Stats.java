b0b8a837ff9bf64d430e2f8f50bcbdbf878e261f
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-27 21:37:08 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -14,7 +14,97 @@ ALGO
     0. 
 */
 
+import java.lang.Math;
+
 public class Stats {
+    
+    public class Stats {
+    public static int mean(int a, int b) {
+        return (a + b) / 2;
+    }
+
+    public static double mean(double a, double b) {
+        return (a + b) / 2;
+    }
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static double max(double a, double b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    /*
+    public static int geoMean(int a, int b) {
+        if (a >= 0 && b >= 0) {
+            int x = Math.pow(a * b, 1/2);
+            return x;
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1;
+        }
+    }
+    */
+
+    public static double geoMean(double a, double b) {
+        if (a >= 0 && b >= 0) {
+            return Math.pow(a * b, 1.0 / 2);
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1.0;
+        }
+    }
+
+    public static int max(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static double max(double a, double b, double c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    /*
+    public static int geoMean(int a, int b, int c) {
+        if (a >= 0 && b >= 0) {
+            int x = Math.pow(a * b * c, 1/3);
+            return x;
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1;
+        }
+    }
+    */
+
+    public static double geoMean(double a, double b, double c) {
+        if (a >= 0 && b >= 0) {
+            return Math.pow(a * b * c, 1.0 / 3);
+        } else {
+            System.out.println("geoMean() does not work with negative inputs");
+            return -1;
+        }
+    }
+    
     public static int gcd (int a, int b){
         int counter = 0;
         int gcd = 0;
