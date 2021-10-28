3d8e18e8890e3a84265619881cca2313e181a81a
JeffordShau <jshau30@stuy.edu>
2021-10-28 12:31:11 -0400

    27

--- 27/b.java
+++ 27/b.java
@@ -1,1 +1,23 @@
-e
+public class b{\
+
+
+  public static String fenceF(int posts){
+    String post = "|";
+    if (posts == 0){
+      return "";
+    }
+    for(int i = 1; i < posts; i++){
+      post += "--|";
+    }
+    return post;
+  }
+
+  public static String reverseF(String s){
+    for(int count = s.length; count > 0; count--;){
+      
+    }
+  }
+
+
+
+}
