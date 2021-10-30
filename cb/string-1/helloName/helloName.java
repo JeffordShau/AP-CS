0a4eef3938930c106ea34e78addc89e8690f9510
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-30 12:34:27 -0400

    Update helloName.java

--- cb/string-1/helloName/helloName.java
+++ cb/string-1/helloName/helloName.java
@@ -6,7 +6,7 @@ HW28 -- Coding Bat
 */
 
 /*
-Given a string, return a string where for every char in the original, there are two chars.
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
 */
 
 public class helloName {
