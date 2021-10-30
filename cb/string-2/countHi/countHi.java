47a3e358953b5b6849ce5cd45bcdb777b377c00d
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-30 12:22:45 -0400

    Create countHi.java

---
+++ cb/string-2/countHi/countHi.java
@@ -0,0 +1,35 @@
+/*
+Jefford Shau
+APCS
+HW28 -- Coding Bat
+2020-10-30
+*/
+
+/*
+Given a string, return a string where for every char in the original, there are two chars.
+*/
+
+public class countHi {
+
+  public int countHi(String str) {
+    int dogCounter = 0;
+    int catCounter = 0;
+    for(int count = 0; count < str.length() - 2; count ++) {
+      if (str.substring(count, count + 3).equals("cat")) {
+        catCounter ++;
+      }
+      if (str.substring(count, count + 3).equals("dog")) {
+        dogCounter ++;
+      }
+    }
+    return catCounter == dogCounter;
+}
+
+}
+
+  public static void main(String[] args) {
+    System.out.println(helloName("Bob")); // → "Hello Bob!"
+    System.out.println(helloName("Alice")); // → "Hello Alice!"
+    System.out.println(helloName("X")); // → "Hello X!"
+  }
+}
