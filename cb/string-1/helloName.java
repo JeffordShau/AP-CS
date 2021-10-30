d6a485a94937977881b3dd6883175e87a3e1cc4d
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-10-30 12:06:37 -0400

    Create helloName.java

---
+++ cb/string-1/helloName.java
@@ -0,0 +1,24 @@
+/*
+Jefford Shau
+APCS
+HW28 -- Coding Bat
+2020-10-30
+*/
+
+/*
+Given a string, return a string where for every char in the original, there are two chars.
+*/
+
+public class doubleChar {
+
+  public String helloName(String name) {
+  return "Hello " + name + "!";
+  }
+}
+
+  public static void main(String[] args) {
+    System.out.println(helloName("Bob")); // → "Hello Bob!"
+    System.out.println(helloName("Alice")); // → "Hello Alice!"
+    System.out.println(helloName("X")); // → "Hello X!"
+  }
+}
