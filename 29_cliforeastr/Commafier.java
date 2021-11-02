1fb5a2cd2e84cc0319da9aa5fab1b42a66ba035b
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-01 21:03:00 -0400

    Update Commafier.java

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -7,14 +7,13 @@
 /*
 DISCOVERIES
     0.
-
 UNRESOLVED QUESTIONS
     0.
 */
 
 public class Commafier {
     public static void main(String[] args) {
-        
+
         System.out.println(commafyF(1));
         System.out.println(commafyF(10));
         System.out.println(commafyF(101));
@@ -25,7 +24,7 @@ public class Commafier {
         System.out.println(commafyR(101));
         System.out.println(commafyR(1000));
         System.out.println(commafyR(12345));
-        
+
 
         for (String arg : args) {
             System.out.println(commafyF(Integer.parseInt(arg)));
@@ -36,21 +35,24 @@ public class Commafier {
     public static String commafyF(int n) {
         String numberString = "" + n;
         String commaedString = "";
-        
-        int numberStringLength = numberString.length();
 
-        
+        int numberStringLength = numberString.length();
+        int leadingDigits = numberStringLength % 3;
+        int periods = numberStringLength / 3;
+
         if (numberStringLength > 3) {
-            if (numberStringLength % 3 != 0) {
-            commaedString = numberString.substring(0, numberStringLength % 3) + ",";
+            if (leadingDigits != 0) {
+            commaedString = numberString.substring(0, leadingDigits);
             }
-            for (int count = 0; count <= numberStringLength / 3; count++) {
-                commaedString = commaedString + numberString.substring(3 * count, 3 * (count + 1)) + ",";
+            for (int count = 0; count < periods; count++) {
+                commaedString = commaedString + "," + numberString.substring(leadingDigits + 3 * count, leadingDigits + 3 * (count + 1));
             }
+            return commaedString;
+        } else {
+          return numberString;
         }
-        return commaedString; 
     }
-                    
+
 
     public static String commafyR(int n) {
         if (n < 1000) {
