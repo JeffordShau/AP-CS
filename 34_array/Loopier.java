d91328bcdffbe89a3db33f619bbd41df59a537cf
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-13 00:30:25 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -12,71 +12,95 @@ QCC:
   0.
 */
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Loopier {
-	public static int[] slice (int[] array, int begin, int end) {
-		int[] ans = new int[end - begin];
-		for (int idx = begin; idx < end; idx++ ) {
-			ans[idx - begin] = array[idx];
-		}
-		return ans;
-	}
+  public static int[] init(int[] array) {
+    for(int i = 0; i < array.length; i ++) {
+      array[i] = (int)(Math.random() * 64920);
+    }
+    return array;
+  }
 
-	public static int[] init (int[] array) {
-		int l = array.length;
-		for (int x = 0; x < l; x++) {
-			array[x] = (int) (Math.random() * 69420);
-		}
-		return array;
-	}
+  public static String toString(int[] array) {
+    String arrayString = "[";
+    for(int i = 0; i < array.length; i++) {
+      arrayString += array[i];
+      if (i != array.length - 1) {
+        arrayString += ", ";
+      }
+    }
+    return arrayString + "]";
+  }
 
-	public static String toString (int[] array) {
-		String ans = "[";
-		int l = array.length;
-		for (int x = 0; x < l; x++) {
-			int b = array[x];
-			ans += Integer.toString(b);
-			if (!(x == l - 1)) {
-				ans += ", ";
-			}
-		}
-		ans += "]";
-		return ans;
-	}
+  public static int linSearch(int[] a, int target) {
+    for(int i = 0; i < a.length; i ++) {
+      if (a[i] == target) {
+        return i;
+      }
+    }
+    return -1;
+  }
 
-	public static int linSearch (int[] array, int target) {
-		int l = array.length;
-		for (int x = 0; x < l; x++) {
-			if (array[x] == target) {
-				return x;
-			}
-		}
-		return -1;
-	}
+  public static int linSearchR(int[] a, int target) {
+    int idx = a.length;
+    if (a[0] == target) {
+      return idx;
+    }
+    else if (a.length == 1) {
+      return -1;
+    }
+    else {
+      idx -= 1;
+      a = Arrays.copyOf(a, a.length - 1);
+      return linSearchR(a, target);
+    }
+  }
 
-	public static int linSearchR (int[] array, int target) {
-		if (array[0] == target) {
-			return 0;
-		}
-		else {
-			if (array.length == 1) {
-				return -1;
-			}
-			else {
-				array = slice(array, 1);
-				return linSearchR(array, target);
-			}
-		}
-	}
+  public static int freq(int[] a, int target) {
+    int counter = 0;
+    for(int i = 0; i < a.length; i ++) {
+      if (a[i] == target) {
+        counter += 1;
+      }
+    }
+    return counter;
+  }
 
-	public static void main (String[] args) {
-		int[] test = new int[3];
+  public static void main(String[] args) {
+    //test init and toString method
+    int[] init1 = new int[3];
+    System.out.println("init1 = " + toString(init(init1)));
+    int[] init2 = new int[4];
+    System.out.println("init1 = " + toString(init(init2)));
+    int[] init3 = new int[5];
+    System.out.println("init1 = " + toString(init(init3)));
 
-		System.out.println(toString(init(test)));
+    //test linSearch methods
+    int[] linSearch1 = {1, 3, 5, 7, 9};
+    System.out.println(linSearch(linSearch1, 7));
+    // System.out.println(linSearchR(linSearch1, 7));
 
-		int[] test1 = {5, 7, 9, 11};
+    //test freq methods
+    int[] freq1 = {1, 1, 5, 6, 8, 9, 9, 1, 9, 6, 1, 1, 10};
+    System.out.println(freq(freq1, 9));
+    System.out.println(freq(freq1, 1));
 
-		System.out.println(linSearchR(test1, 11));
-	}
+  }
+
+
 }
+
+
+
+/*
+
+Two public, static implementations of a linear search function that will return the index of the first occurrence of a target in an existing array, or -1 if not found. (First version iterates, second recurses.)
+
+ int linSearch( int[] a, int target )
+ int linSearchR( int[] a, int target )
+Two public, static implementations of a frequency function that will return the number of occurrences of a target in an existing array. (First version iterates, second recurses.)
+
+ int freq( int[] a, int target )
+ int freqRec( int[] a, int target )
+ */
