4051753a783bd8cc830f9465e7200fd23185041d
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 23:04:43 -0500

    Update maxEnd3.java

--- cb/array-1/maxEnd3/maxEnd3.java
+++ cb/array-1/maxEnd3/maxEnd3.java
@@ -15,3 +15,5 @@ public class maxEnd3 {
     System.out.println(maxEnd3([1, 2, 3]) // [3, 3, 3]
     System.out.println(maxEnd3([11, 5, 9]) // [11, 11, 11]
     System.out.println(maxEnd3([2, 11, 3]) // [3, 3, 3]
+ }
+                       }
