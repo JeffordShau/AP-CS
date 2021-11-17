2161d912f367b60a542ae22ca26ef17395f3304e
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 22:12:38 -0500

    Update sameFirstLast.java

--- cb/array-1/sameFirstLast/sameFirstLast.java
+++ cb/array-1/sameFirstLast/sameFirstLast.java
@@ -4,15 +4,15 @@ Given an array of ints, return true if the array is length 1 or more, and the fi
 
 public class sameFirstLast {
   public static boolean sameFirstLast(int[] nums) {
-  if (nums.length >= 1) {
-    return nums[0] == nums[nums.length - 1]; 
-  }
-  return false;
+    if (nums.length >= 1) {
+      return nums[0] == nums[nums.length - 1]; 
+    }
+    return false;
   }
   
   public static void main(String[] args) {
-    System.out.println(sameFirstLast([1, 2, 3]);
-    System.out.println(sameFirstLast([1, 2, 3, 1]);
-    System.out.println(sameFirstLast([1, 2, 1]);
+    System.out.println(sameFirstLast([1, 2, 3]); //false
+    System.out.println(sameFirstLast([1, 2, 3, 1]); // true
+    System.out.println(sameFirstLast([1, 2, 1]); // true
   }
 }
