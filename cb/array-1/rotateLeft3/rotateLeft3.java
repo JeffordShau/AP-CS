6dfd0c5e4a97ac98e2725df907c97881a1ee30b7
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 23:05:41 -0500

    Update rotateLeft3.java

--- cb/array-1/rotateLeft3/rotateLeft3.java
+++ cb/array-1/rotateLeft3/rotateLeft3.java
@@ -11,3 +11,5 @@ public class rotateLeft3 {
     System.out.println(rotateLeft3([1, 2, 3]); // [2, 3, 1]
     System.out.println(rotateLeft3([5, 11, 9]); // [11, 9, 5]
     System.out.println(rotateLeft3([7, 0, 0]); // [0, 0, 7]
+ }
+                       }
