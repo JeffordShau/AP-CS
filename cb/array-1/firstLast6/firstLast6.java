19333c7b365e23e9f98c5d28fe16ea29fcf76498
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 22:11:19 -0500

    Update firstLast6.java

--- cb/array-1/firstLast6/firstLast6.java
+++ cb/array-1/firstLast6/firstLast6.java
@@ -7,6 +7,6 @@ public class firstLast6 {
     return (nums[0] == 6 || nums[nums.length - 1] == 6); 
     
   public static void man(String[] args) {
-    System.out.println(firstLast6([1, 2, 6])); 
-    System.out.println(firstLast6([6, 1, 2, 3])); 
-    System.out.println(firstLast6([13, 6, 1, 2, 3])); 
+    System.out.println(firstLast6([1, 2, 6])); // true
+    System.out.println(firstLast6([6, 1, 2, 3])); // true
+    System.out.println(firstLast6([13, 6, 1, 2, 3])); // false
