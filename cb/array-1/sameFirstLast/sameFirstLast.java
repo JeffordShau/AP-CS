ed0af0428f33fc94d7fefcac74c78b3cbdaf83b4
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 22:01:57 -0500

    Update sameFirstLast.java

--- cb/array-1/sameFirstLast/sameFirstLast.java
+++ cb/array-1/sameFirstLast/sameFirstLast.java
@@ -1,3 +1,7 @@
+/*
+Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+*/
+
 public class sameFirstLast {
   public static boolean sameFirstLast(int[] nums) {
   if (nums.length >= 1) {
