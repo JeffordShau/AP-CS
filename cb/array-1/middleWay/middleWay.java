a6983685df827b942611f3354976bac9a9d77fdd
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 23:04:33 -0500

    Update middleWay.java

--- cb/array-1/middleWay/middleWay.java
+++ cb/array-1/middleWay/middleWay.java
@@ -11,3 +11,5 @@ public class middleWay {
     System.out.println(middleWay([1, 2, 3], [4, 5, 6]); // [2, 5]
     System.out.println(middleWay([7, 7, 7], [3, 8, 0]); // [7, 8]
     System.out.println(middleWay([5, 2, 9], [1, 4, 5]); // [2, 4]
+ }
+                       }
