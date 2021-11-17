08962636c83f985c166f3749b16d47c7ee2dd250
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 22:15:24 -0500

    Create sum3.java

---
+++ cb/array-1/sum3/sum3.java
@@ -0,0 +1,9 @@
+public class sum3 {
+  public static sum3(int[] nums) {
+    return nums[0] + nums[1] + nums[2];
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(sums([1, 2, 3]); // 6
+    System.out.println(sums([5, 11, 2]); // 18
+    System.out.println(sums([7, 0, 0]); // 7            
