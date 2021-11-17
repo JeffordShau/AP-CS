59e503337093bf29dc66dfa58f13c985d04a4859
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-16 22:25:39 -0500

    Create maxEnd3.java

---
+++ cb/array-1/maxEnd3/maxEnd3.java
@@ -0,0 +1,17 @@
+/*
+Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+*/
+
+public class maxEnd3 {
+  public static int[] maxEnd3(int[] nums) {
+    if (nums[0] >= nums[2]) {
+      return new int[] {nums[0], nums[0], nums[0]}; 
+    } else {
+      return new int[] {nums[2], nums[2], nums[2]}; 
+    }
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(maxEnd3([1, 2, 3]) // [3, 3, 3]
+    System.out.println(maxEnd3([11, 5, 9]) // [11, 11, 11]
+    System.out.println(maxEnd3([2, 11, 3]) // [3, 3, 3]
