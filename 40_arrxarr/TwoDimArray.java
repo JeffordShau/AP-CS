f233e190f264c1ad5776e949091b3a826807bec2
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-11-29 23:11:36 -0500

    Update TwoDimArray.java

--- 40_arrxarr/TwoDimArray.java
+++ 40_arrxarr/TwoDimArray.java
@@ -6,10 +6,10 @@ HW40 -- Demand A Raise
 */
 
 /*
-DISCO: 
-  0. 
-QCC: 
-  0. 
+DISCO:
+  0. Successfully incorporated for and foreach loop to print and sum methods.
+QCC:
+  0. How do you add the commas and the array brackets to the print statement without adding a helper method?
   */
 
 public class TwoDimArray
@@ -18,8 +18,14 @@ public class TwoDimArray
   //          uses a FOR loop
   public static void print1( int[][] a )
   {
-    for(int rows = 0; rows < a.length(); rows++) {
-      System.out.println(rows); 
+    String totalArray = "";
+    for(int rows = 0; rows < a.length; rows++) {
+      for (int idx = 0; idx < a[rows].length; idx++) {
+        System.out.print(a[rows][idx]);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 
 
@@ -27,8 +33,12 @@ public class TwoDimArray
   //          uses a FOREACH loop
   public static void print2( int[][] a )
   {
-    for (int[] rows:a) {
-      System.out.println(rows);
+    for (int[] subarray: a){
+    	for (int elem : subarray){
+    		System.out.print(elem);
+    		System.out.print(" ");
+    	}
+    	System.out.println();
     }
   }
 
@@ -36,7 +46,13 @@ public class TwoDimArray
   //postcond: returns sum of all items in 2D int array a
   public static int sum1( int[][] a )
   {
-    // YOUR IMPLEMENTATION HERE
+    int totalSum = 0;
+    for(int[] rows:a) {
+      for(int elements:rows) {
+        totalSum += elements;
+      }
+    }
+    return totalSum;
   }
 
 
@@ -44,7 +60,11 @@ public class TwoDimArray
   //          * uses helper fxn sumRow
   public static int sum2( int [][] m )
   {
-    // YOUR IMPLEMENTATION HERE
+    int totalSum = 0;
+    for(int rows = 0; rows < m.length; rows++) {
+      totalSum += sumRow(rows, m);
+    }
+    return totalSum;
   }
 
 
@@ -52,7 +72,11 @@ public class TwoDimArray
   //          uses a FOR loop
   public static int sumRow( int r, int[][] a )
   {
-    // YOUR IMPLEMENTATION HERE
+    int rSum = 0;
+    for(int idx = 0; idx < a[r].length; idx++) {
+      rSum += a[r][idx];
+    }
+    return rSum;
   }
 
 
@@ -62,13 +86,15 @@ public class TwoDimArray
   {
     // *** YOUR IMPLEMENTATION HERE ***
     int summer = 0;
+    for(int idx:m[r]) {
+      summer += idx;
+    }
     return summer;
   }
 
 
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       int [][] m1 = new int[4][2];
       int [][] m2 = { {2,4,6}, {3,5,7} };
       int [][] m3 = { {2}, {4,6}, {1,3,5} };
@@ -78,7 +104,6 @@ public class TwoDimArray
       print2(m1);
       print2(m2);
       print2(m3);
-          /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       System.out.print("testing sum1...\n");
       System.out.println("sum m1 : " + sum1(m1));
       System.out.println("sum m2 : " + sum1(m2));
@@ -87,7 +112,8 @@ public class TwoDimArray
       System.out.println("sum m1 : " + sum2(m1));
       System.out.println("sum m2 : " + sum2(m2));
       System.out.println("sum m3 : " + sum2(m3));
-      
+      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     // your own custom test cases welcomed and encouraged
