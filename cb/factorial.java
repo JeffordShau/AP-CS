1911f08865cfb1a315135a042927cf008b5d4743
JeffordShau <jshau30@stuy.edu>
2022-02-14 16:19:21 -0500

    classwork progress

---
+++ cb/factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+  public static int factorial(int n) {
+    if (n == 0) {
+      return 0;
+    }
+    else if (n == 1) {
+      return 1;
+    }
+    return n * factorial(n-1);
+  }
+}
