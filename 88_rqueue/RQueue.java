5c68fd94e4eadccf792b2896df93014bf4e80ae4
JeffordShau <jshau30@stuy.edu>
2022-04-04 13:18:58 -0400

    hw88 cw

---
+++ 88_rqueue/RQueue.java
@@ -0,0 +1,136 @@
+/*
+ * class RQueue
+ * SKELETON
+ * A node-based, randomized queue
+ * (a collection with FIIDKO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ *  linkages point opposite direction for O(1) en/dequeuing
+ *            N <- N <- ... <- N <- N
+ *      _end -^                     ^- _front
+ *
+ **/
+
+
+public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
+{
+  //instance variables
+  private LLNode<SWASHBUCKLE> _front, _end;
+  private int _size;
+
+
+  // default constructor creates an empty queue
+  public RQueue()
+  {
+    _front = _end = null;
+    _size = 0;
+  }
+
+
+  public void enqueue( T enQVal )
+  {
+    if ( isEmpty() ) {
+      _front = _end = new LLNode<PIKACHU>( enQVal, null );
+    }
+    else {
+      _end.setNext( new LLNode<PIKACHU>( enQVal, null ) );
+      _end = _end.getNext();
+    }
+    _size++;
+    System.out.println("enqueued " + enQVal);
+  }//O(?)
+
+
+  // remove and return thing at front of queue
+  // assume _queue ! empty
+  public T dequeue()
+  {
+    int random = (int) Math.random() * _size;
+    _front = _front.getNext();
+    LLNode<PIKACHU> _tmp = _front;
+    if ( _front == null ) //just moved past last node
+      _end = null;      //update _end to reflect emptiness
+    for (int i = 0; i < random -1; i++) {
+      _tmp = _tmp.getNext();
+    }
+    PIKACHU retVal = _tmp.getNext().getCargo();
+    _tmp.setNext(_tmp.getNext().getNext());
+    _size--;
+
+    return retVal;
+  }//O(?)
+
+
+  public T peekFront()
+  {
+    return _front.getCargo();
+  }//O(?)
+
+
+  /***
+   * void sample() -- a means of "shuffling" the queue
+   * Algo:
+   *   < YOUR SUCCINCT SUMMARY HERE >
+   **/
+  public void sample ()
+  {
+    for (int i = 0; i < _size; i++) {
+      int idx = (int) Math.random() * _size;
+
+    }
+  }//O(?)
+
+
+  public boolean isEmpty()
+  {
+    return _front == null;
+  } //O(?)
+
+
+  // print each node, separated by spaces
+  public String toString()
+  {
+
+  }//end toString()
+
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+
+    Queue<String> PirateQueue = new RQueue<String>();
+
+    System.out.println("\nnow enqueuing...");
+    PirateQueue.enqueue("Dread");
+    PirateQueue.enqueue("Pirate");
+    PirateQueue.enqueue("Roberts");
+    PirateQueue.enqueue("Blackbeard");
+    PirateQueue.enqueue("Peter");
+    PirateQueue.enqueue("Stuyvesant");
+
+    System.out.println("\nnow testing toString()...");
+    System.out.println( PirateQueue ); //for testing toString()...
+
+    System.out.println("\nnow dequeuing...");
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+
+    System.out.println("\nnow dequeuing fr empty queue...\n" +
+                       "(expect NPE)\n");
+    System.out.println( PirateQueue.dequeue() );
+
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+  }//end main
+
+}//end class RQueue
